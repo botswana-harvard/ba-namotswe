f587f8e239601181f53962df72ef05c3a3d8bcb5
erikvw <ew2789@gmail.com>
2016-11-21 02:31:00 +0200

    update some refs, others pending ...

--- ba_namotswe/visit_schedules.py
+++ ba_namotswe/visit_schedules.py
@@ -1,7 +1,7 @@
-from edc_visit_schedule.site_visit_schedules import site_visit_schedules
-from edc_visit_schedule.visit_schedule import VisitSchedule
 from edc_visit_schedule.schedule import Schedule
+from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 from edc_visit_schedule.visit import Crf
+from edc_visit_schedule.visit_schedule import VisitSchedule
 
 entry_to_care_crfs = (
     Crf(show_order=10, model='ba_namotswe.entrytocare'),
